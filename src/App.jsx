97b57f46c13dc0194f51517a0d3d9c8f0d17c5fb
Alice <alicesb2006@gmail.com>
2024-08-13 09:22:08 -0300

    Criando a estrutura do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import Result from "./components/Result"
+import { useState, useEffect } from "react" //Hooks
 
 function App() {
 
+
+
   return (
     <>
-      <form>
-        <div>
-          <label htmlFor="altura">Altura: <span className=""></span></label>
-        </div>
-      </form>
+      <div className="container">
+          <div className="box">
+            <form>
+              <div>
+                <label htmlFor="altura">Altura: <span className="span">(Exemplo: 1.80)</span></label>
+                <input type="number" id="altura" placeholder="Digite a sua Altura" onBlur={({target}) => parseFloat(setAltura(target.value))} />
+              </div>
+              <div>
+                <label htmlFor="peso">Peso: <span className="span">(Exemplo: 50)</span></label>
+                <input type="number" id="peso" placeholder="Digite o seu Peso" onBlur={({target}) => parseInt(setPeso(target.value))} />
+              </div>
+              <button type="submit" onClick={calcularIMC()}>Calcular</button>
+            </form>
+          </div>
+
+          {mostrarResultado &&(
+            //Definindo uma props
+            <Result resultado = {resultado} />
+          )}
+
+      </div>
     </>
   )
 }
